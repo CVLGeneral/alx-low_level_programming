ca62215cd414d173d6c031c9f6fdeeabf15497d4
CVLGeneral <91874660+CVLGeneral@users.noreply.github.com>
2022-07-22 21:32:50 +0300

    My Task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,20 +1,14 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- *main - prints the program name
- *@argc: captures the number of commands in the command line
- *@argv: captures the strings in the command line
- *Return: always 0
+ * main - prints the name of the program
+ * @argc: number of arguement
+ * @argv: 1Dimensional array of strings
+ * Return: Always 0
  */
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-	int i;
-
-	for (i = 0; i < argc; i++)
-	{
-		printf("%s\n", argv[i]);
-	}
+	printf("%s\n", argv[argc - 1]);
 	return (0);
 }
