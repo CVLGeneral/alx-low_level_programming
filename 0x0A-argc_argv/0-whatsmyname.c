8bb3e8db2d01442a979929478b124039b4cf3dae
CVLGeneral <91874660+CVLGeneral@users.noreply.github.com>
2022-07-22 21:02:52 +0300

    My Commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,14 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - Prints the program name, followed by a new line.
- * @argc: The number of arguments supplied to the program.
- * @argv: An array of pointers to the arguments.
+ * main - Entry point
+ * @argc: number of params
+ * @argv: array of params
  *
- * Return: Always o.
+ * Return: Always 0 (Success)
  */
-int main(int __attribute__((__unused__)) argc, char *argv[])
+
+int main(__noerr int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 
