ace0f9de9c3a6a1abec37c82b91276c69c858186
CVLGeneral <91874660+CVLGeneral@users.noreply.github.com>
2022-07-22 20:23:11 +0300

    My Commit

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,51 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 /**
- *main - funtion
- *@argc: legenth of argv
- *@argv: number of argummet
- *Return: Always 0
+ * main - Prints the program name, followed by a new line.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: Always o.
  */
-
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	/*Declaring variables*/
-	int position, total, change, aux;
-	int coins[] = {25, 10, 5, 2, 1}; /*Array int*/
+	printf("%s\n", argv[0]);
 
-	position = total = change = aux = 0;
-
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-
-	total = atoi(argv[1]); /*Convert str to int*/
-
-	if (total <= 0)
-	{
-		printf("0\n");
-		return (0);
-	}
-
-	/*Declaring WHILE*/
-
-	while (coins[position] != '\0')
-
-	{
-		if (total >= coins[position])
-		{
-			aux = (total / coins[position]);
-			change += aux;
-			total -= coins[position] * aux;
-		}
-
-		position++;
-
-	}
-
-	printf("%d\n", change);
 	return (0);
 }
