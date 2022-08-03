98eb5578a96db588ef255d4781a2bd0b728fb98d
CVLGeneral <91874660+CVLGeneral@users.noreply.github.com>
2022-08-03 19:41:02 +0300

    My Commit

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,38 @@
-#include <stdio.h>
+#ifndef _FUNCTION_POINTER_H
+#define _FUNCTION_POINTER_H
 
-#ifndef FUNCTION_POINTERS_
-#define FUNCTION_POINTERS_
+#include <stddef.h>
+
+/**
+ * print_name - prints a name
+ * @name: string
+ * @f: pointer to a func
+ *
+ * Return: void
+ */
 
 void print_name(char *name, void (*f)(char *));
+
+/**
+ * array_iterator - execute a func on each array
+ * @array: array
+ * @size: size of array
+ * @action: pointer to a func
+ *
+ * Return: void
+ */
+
 void array_iterator(int *array, size_t size, void (*action)(int));
+
+/**
+ * int_index - search for an integer
+ * @array: array
+ * @size: size
+ * @cmp: func pointer
+ *
+ * Return: int
+ */
+
 int int_index(int *array, int size, int (*cmp)(int));
 
-#endif /* #ifndef FUNCTION_POINTERS_ */
+#endif
